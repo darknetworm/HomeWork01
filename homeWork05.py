44cc49834f2ddde1e6d83b09e4686b40a05eab91
darknetworm <82410807+darknetworm@users.noreply.github.com>
2021-04-13 21:06:11 +0300

    Расчет прибыли/убыли фирмы

---
+++ homeWork05.py
@@ -0,0 +1,10 @@
+income = int(input('Введите выручку фирмы: '))
+outcome = int(input('Введите издержки фирмы: '))
+
+if outcome > income:
+    print('Фирма работает в убыток')
+else:
+    print('Фирма приносит прибыль с рентабельностью: ', income / outcome)
+    personal = int(input('Введите численность сотрудников фирмы: '))
+    print('Прибыль фирмы в расчете на одного сотрудника составляет: ', income/personal)
+    
